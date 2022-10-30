fa06d21953e64c811b70c7e3aada20b26a3bfe76
Dastan <super.dastno@yandex.ru>
2022-10-30 10:33:24 +0300

    Add 1 ex

--- Python/Practice_5/Ex_01.py
+++ Python/Practice_5/Ex_01.py
@@ -1,141 +1,109 @@
-# Создайте программу для игры с конфетами человек против человека.
-# Условие задачи: На столе лежит 2021 конфета. Играют два игрока делая ход друг после друга. 
-# Первый ход определяется жеребьёвкой. За один ход можно забрать не более чем 28 конфет. 
-# Все конфеты оппонента достаются сделавшему последний ход. Сколько конфет нужно взять первому игроку,
-# чтобы забрать все конфеты у своего конкурента?
-# a) Добавьте игру против бота
+# Создайте программу для игры в ""Крестики-нолики"".
 
 from os import system
 from random import randint
-from math import floor
+
+# Вывод игрового поля в консоль
+def array_print(arr):
+    for i in range(len(arr)):
+        print(arr[i])
 
 
-# Начало игры
-def start_game():
-    print("""
-    Добро пожаловать в игру!
-    Выберите вариант игры:
-    1. С компьютером.
-    2. Два игрока.
-    3. С "умным" компьютером. """)
-    a=0
-    while a<1 or a>3:
-        a = int(input("    Что выбираем? "))
-    return a
+# Проверка игрового поля на выигрыш
+def check_field(array, player_symbol):
+    count = [0,0,0,0]
+    for i in range(len(array)):
+        for j in range(len(array[i])):
+            if array[j][i]==player_symbol:
+                count[0]+=1
+            if array[i][j]== player_symbol:
+                count[1]+=1
+        if count[0]!=len(array):
+            count[0] = 0
+        else:
+            return count[0]
+        if count[1]!= len(array):
+            count[1] = 0
+        else:
+            return count[1]
+        if array[i][i]==player_symbol:
+            count[2]+=1
+        if array[len(array)-1-i][i]==player_symbol:
+            count[3]+=1
+    for i in range(len(count)):
+        if count[i] == len(array):
+            return count[i]
+    return 0
 
-# Режим двух игроков - пользователей     
-def two_players(cand):
-    candies = cand
-    # 0 индекс - это компьютер, 1 игрок - это пользователь
-    players= [0,0]
-    move = 29
-    select=randint(0,1)
-    print('Итак, на столе лежит 2021 конфета')
-    while candies!=0:
-        while move>28:
-            print(f'Игрок {select+1}, Ваш ход.')
-            move = int(input(f'Сколько конфет заберем? Помни, не больше 28!    -   Осталось {candies} конфет.  '))    
-        if candies-move>=0:
-            players[select]+=move
-            candies -= move
-            if candies != 0:
-                select = len(players)-1-select
-        move=29
-    return select+1
 
-# Режим игры с компьютером
-def bot_player(cand):
-    candies = cand
-    # 0 индекс - это компьютер, 1 игрок - это пользователь
-    players = [0,0]
-    move=29
-    select = randint(0,1)
-    print('Итак, на столе лежит 2021 конфета')
-    while candies!=0:
-        while move>28:
-            if select == 1:
-                print(f'Игрок, Ваш ход.')
-                move = int(input(f'Сколько конфет заберем? Помни, не больше 28!    -   Осталось {candies} конфет.  ')) 
-                print('___________________________________________')
-            else:
-                print('Ход компьютера')
-                if candies>=28:
-                    move = randint(1,28)
-                    print(f'Компьютер забирает {move} конфет.                      -   Осталось {candies-move} конфет.\n___________________________________________')
-                else:
-                    move = candies
-                    print(f'Компьютер забирает {move} конфет.                      -   Осталось {candies-move} конфет.\n___________________________________________')
-
-        if candies-move>=0:
-            players[select]+=move
-            candies -= move
-            if candies != 0:
-                select = len(players)-1-select
-        move=29
-    return select+1
-
-# Режим игры с "умным" компьютером
-def clever_bot_player(cand):
-    candies = cand
-    # 0 индекс - это компьютер, 1 игрок - это пользователь
-    players = [0,0]
-    move=29
-    select = randint(0,1)
-    print(f'Итак, на столе лежит {candies} конфета')
-    while candies!=0:
-        while move>28:
-            if select == 1:
-                print(f'Игрок, Ваш ход.')
-                move = int(input(f'Сколько конфет заберем? Помни, не больше 28!    -   Осталось {candies} конфет.  ')) 
-                print('___________________________________________')
-            else:
-                print('Ход компьютера')
-                if candies > 28:
-                    move=candies - floor(candies / move)*move
-                    print(f'Компьютер забирает {move} конфет.                      -   Осталось {candies} конфет.\n___________________________________________')
-                else:
-                    move = candies
-                    print(f'Компьютер забирает {move} конфет.                      -   Осталось {candies} конфет.\n___________________________________________')
-
-        if candies-move>=0:
-            players[select]+=move
-            candies -= move
-            if candies != 0:
-                select = len(players)-1-select
-        move=29
-    return select+1
-
-# Ход игры
-def game(choice):
-    total_candies = 2021
-    if choice == 2:
-        win = two_players(total_candies)
-    elif choice ==1:
-        win = bot_player(total_candies)
+# Проверка ячеек игрового поля на попытку повторного ввода
+def check_symbol(array, position):
+    if position[0]==0 and position[1]==0:
+        position = [1,1]
+    if array[position[0]-1][position[1]-1]=='_':
+        return True
     else:
-        win = clever_bot_player(total_candies)
+        return False
 
-    return win
+# Проверка игрового поля на полное заполнение
+def check_array(arr):
+    check = False
+    for i in range(len(arr)):
+        for j in range(len(arr[i])):
+            if arr[i][j]=='_':
+                check = True
+                return check
+    return check
+
 
 # Очистка консоли
 clear = lambda: system('cls')
 
-# Печать информации о победителе
-def print_winner(choice,player):
-    type = choice
-    winner = player
-    if type == 1 or winner ==1:
-        print('Компьютер выиграл!')
-    elif type==2:
-        print(f'Игрок {winner}-выиграл!!!')
-    elif type ==3 or winner ==1:
-        print('Игрок победил!!!')
+# Создание игрового поля
+def field():
+    size = 0 
+    while size < 3 or size > 5:
+        size = int(input('Добро пожаловать в игру "Крестики-нолики"\nКакого размера будет поле? Введите цифру от 3 до 5: '))
+    array = [['_']*size for i in range(size)]
+    return array
 
-# Игра
+# Ход игры
+def fill_field(array):
+    game_field = array
+    select = randint(0,1)
+    players = ['X','O']
+    move = [0,0]
+    end = 0
+    while end != len(game_field):
+        clear()
+        array_print(game_field)
+        print(f'Игрок {select+1}, Ваш ход')
+        while move[0]<1 or move[0]>len(game_field) or move[1]<1 or move[1]> len(game_field) or check_symbol(game_field,move)==False:
+            move = [int(i) for i in (input("Введите номер строки и столбца для заполнения через пробел: ").split())]
+            if check_array(game_field)== False:
+                break
+        game_field[move[0]-1][move[1]-1] = players[select]
+        array_print(game_field)
+        end = check_field(game_field,players[select])
+        if end !=len(game_field):
+            if check_array(game_field)==False:
+                select = 2
+                clear()
+                array_print(game_field)
+                return select
+            select = len(players)-1-select
+            move=[0,0]
+    clear()
+    array_print(game_field)
+    return select
+    
+#Главное меню
 def Main():
     clear()
-    type = start_game()
-    winner = game(type)
-    print_winner(type,winner)
-
-Main()        
+    g_field = field()
+    winner = fill_field(g_field)+1
+    if winner == 3:
+        print('Ничья! Победителей нет!')
+    else:
+        print(f'Игрок {winner} - победитель')
+Main()
